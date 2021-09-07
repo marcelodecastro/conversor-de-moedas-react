9c1598ee3134f7bf1db86b4c11521beb7a20d64e
Marcelo de Castro <53918398+marcelodecastro@users.noreply.github.com>
2021-09-07 15:29:45 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+
+import './App.css';
+
+import Conversor from "./components/Conversor"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Conversor de moedas $</h1>
+
+      <div className="linha">
+      <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+      <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+      </div>
+      
+      <div className="linha">
+      <Conversor moedaA="CAD" moedaB="BRL"></Conversor>
+      <Conversor moedaA="BRL" moedaB="CAD"></Conversor>  
+      </div>
+
+      <div className="linha">
+      <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
+      <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
+      </div>
+
+      <div className="linha">
+      <Conversor moedaA="GBP" moedaB="BRL"></Conversor>
+      <Conversor moedaA="BRL" moedaB="GBP"></Conversor>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
